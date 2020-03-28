ba386e109eb34f9105c79bf5f42751a1998e268f
rk717 <55735239+rk717@users.noreply.github.com>
2020-03-28 23:00:47 +0100

    Create abBA" to "AaBb.py

---
+++ abBA" to "AaBb.py
@@ -0,0 +1,19 @@
+Test.describe("Basic tests")
+
+Test.assert_equals(find_children("abBA"),"AaBb")
+
+Test.assert_equals(find_children("AaaaaZazzz"),"AaaaaaZzzz")
+
+Test.assert_equals(find_children("CbcBcbaA"),"AaBbbCcc")
+
+Test.assert_equals(find_children("xXfuUuuF"),"FfUuuuXx")
+
+Test.assert_equals(find_children(""),"")
+
+
+def find_children(s):
+    a = ''.join(sorted(s))
+    b = ''.join(sorted(a,key = str.lower))
+    return b
+            
+print(find_children("abBAdcDC"))
