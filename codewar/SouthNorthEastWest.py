fa1fb24b4fea53061524d7614e591d66420b007c
rk717 <55735239+rk717@users.noreply.github.com>
2020-04-02 19:08:50 +0200

    Create SouthNorthEastWest.py

---
+++ codewar/SouthNorthEastWest.py
@@ -0,0 +1,18 @@
+a = ["NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"]
+test.assert_equals(dirReduc(a), ['WEST'])
+u=["NORTH", "WEST", "SOUTH", "EAST"]
+test.assert_equals(dirReduc(u), ["NORTH", "WEST", "SOUTH", "EAST"])
+
+
+
+def dirReduc(arr):
+    dict = {"NORTH":"SOUTH","SOUTH":"NORTH","EAST":"WEST","WEST":"EAST"}
+    res = []
+    for i in arr:
+        if res and dict[i] == res[-1]:
+            res.pop()
+        else:
+            res.append(i)
+    return res
+
+print(dirReduc(("NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST")))
