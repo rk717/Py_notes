d42c5d72cc29e88e14b5fa2db64e7466da2d502c
rk717 <55735239+rk717@users.noreply.github.com>
2020-04-04 21:22:32 +0200

    Update replace polish to English.py

--- codewar/replace polish to English.py
+++ codewar/replace polish to English.py
@@ -4,3 +4,15 @@ def correct_polish_letters(st):
     for i in range(len(l)):
         st = st.replace(l[i], lt[i])
     return st
+----------------------------------------------------
+def correct_polish_letters(st): 
+    d={'ą':'a',
+    'ć':'c',
+    'ę':'e',
+    'ł':'l',
+    'ń':'n',
+    'ó':'o',
+    'ś':'s',
+    'ź':'z',
+    'ż':'z'}
+    return "".join(d[c] if c in d else c for c in st)
