c7d2e99c97588477c02667cfce8f4d376beaeca4
rk717 <55735239+rk717@users.noreply.github.com>
2020-04-04 13:07:50 +0200

    Update stringList_int.py

--- codewar/stringList_int.py
+++ codewar/stringList_int.py
@@ -1,3 +1,29 @@
+Conver string to a list. 
+
+# Python code to convert string to list 
+
+def Convert(string): 
+	li = list(string.split(" ")) 
+	return li 
+
+# Driver code	 
+str1 = "Geeks for Geeks"
+print(Convert(str1)) 
+
+# Python code to convert string to list 
+def Convert(string): 
+	li = list(string.split("-")) 
+	return li 
+
+# Driver code	 
+str1 = "Geeks-for-Geeks"
+print(Convert(str1)) 
+
+
+
+
+
+---------------------------------------
 results = ['1', '2', '3']
 >>>results = list(map(int, results))
 
