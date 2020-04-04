3bdadcbf25629e422e0c3df67c45bf9c74d7c184
rk717 <55735239+rk717@users.noreply.github.com>
2020-04-04 14:21:49 +0200

    Update stringList_int.py

--- codewar/stringList_int.py
+++ codewar/stringList_int.py
@@ -35,3 +35,14 @@ results = ['1', '2', '3']
 >>> results
 [1, 2, 3]
 results = [int(i) for i in results]
+
+------------------------------------------
+How to print list in python??
+# Python program to print list 
+# using for loop 
+a = [1, 2, 3, 4, 5] 
+
+# printing the list using loop 
+for x in range(len(a)): 
+	print a[x], 
+
