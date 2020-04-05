bdb4b002dfdf93caa6d92d6b91b20ffc05e14d7e
rk717 <55735239+rk717@users.noreply.github.com>
2020-04-05 14:07:01 +0200

    Create map.py

---
+++ codewar/map.py
@@ -0,0 +1,35 @@
+def addition(n): 
+	return n + n 
+
+numbers = (1, 2, 3, 4) 
+result = map(addition, numbers) 
+print(list(result)) 
+
+#{2, 4, 6, 8}
+
+-------------------------------------
+
+numbers = (1, 2, 3, 4) 
+result = map(lambda x: x + x, numbers) 
+print(list(result)) 
+
+#{2, 4, 6, 8}
+
+--------------------------------------
+
+numbers1 = [1, 2, 3] 
+numbers2 = [4, 5, 6] 
+
+result = map(lambda x, y: x + y, numbers1, numbers2) 
+print(list(result)) 
+
+#[5, 7, 9]
+
+-----------------------------------------
+
+l = ['sat', 'bat', 'cat', 'mat'] 
+
+test = list(map(list, l)) 
+print(test) 
+
+#[['s', 'a', 't'], ['b', 'a', 't'], ['c', 'a', 't'], ['m', 'a', 't']]
