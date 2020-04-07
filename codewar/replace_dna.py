105f9d01d21d34c33789dc6a2a113a46dbfef779
rk717 <55735239+rk717@users.noreply.github.com>
2020-04-07 08:47:23 +0200

    Create replace_dna.py

---
+++ codewar/replace_dna.py
@@ -0,0 +1,38 @@
+Test.assert_equals(DNA_strand("AAAA"),"TTTT","String AAAA is")
+Test.assert_equals(DNA_strand("ATTGC"),"TAACG","String ATTGC is")
+Test.assert_equals(DNA_strand("GTAT"),"CATA","String GTAT is")
+
+DNA_strand ("ATTGC") # return "TAACG"
+
+DNA_strand ("GTAT") # return "CATA"
+
+----------------------------------------------------------------
+
+import string
+def DNA_strand(dna):
+    return dna.translate(string.maketrans("ATCG","TAGC"))
+    # Python 3.4 solution || you don't need to import anything :)
+    # return dna.translate(str.maketrans("ATCG","TAGC"))
+    
+    
+ =================================================================
+def DNA_strand(dna):
+    reference = { "A":"T",
+                  "T":"A",
+                  "C":"G",
+                  "G":"C"
+                  }
+    return "".join([reference[x] for x in dna])
+    
+==================================================================
+def DNA_strand(dna):
+  return "".join([{'A':'T', 'T':'A', 'C':'G', 'G':'C'}[l] for l in dna])
+ 
+===================================================================
+import string
+
+TABLE = string.maketrans('ATCG', 'TAGC')
+
+
+def DNA_strand(dna):
+    return dna.translate(TABLE)
