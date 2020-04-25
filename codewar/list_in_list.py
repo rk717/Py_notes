113c0ee223cc5da32cbf9570a108fce982f2b9d8
rk717 <55735239+rk717@users.noreply.github.com>
2020-04-25 18:59:24 +0200

    Update list_in_list.py

--- codewar/list_in_list.py
+++ codewar/list_in_list.py
@@ -10,3 +10,19 @@ def openOrSenior(data):
 print((openOrSenior([[45, 12],[55,21],[19, -2],[104, 20]])))
 
 #['Open', 'Senior', 'Open', 'Senior']
+
+
+------------------------------------------------------------
+[[10,0],[3,5],[5,8]]
+一串list 里 的list 第一个数的和 减去 第二个数
+def number(bus_stops):
+  a = 0
+  b = 0
+  for i in bus_stops:
+    a += i[0]
+    b += i[1]
+  return a - b
+
+-----------------------------------------------------
+def number(bus_stops):
+    return sum([stop[0] - stop[1] for stop in bus_stops])
