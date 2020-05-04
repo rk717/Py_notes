00f74cff247e8abb40ffdd8148d2424a850fcf07
rk717 <55735239+rk717@users.noreply.github.com>
2020-05-04 19:26:06 +0200

    Update list_dictionary.py

--- codewar/list_dictionary.py
+++ codewar/list_dictionary.py
@@ -38,3 +38,16 @@ def namelist(names):
 
 print(namelist([{'name': 'Bart'},{'name': 'Lisa'},{'name': 'Maggie'}]))
 #['Bart', 'Lisa', 'Maggie']
+#############################################################
+def namelist(names):
+    namelist = [name['name'] for name in names]
+    if len(namelist) <= 1:
+        return "".join(namelist)
+    else:
+        lastTwo = " & ".join(namelist[-2:])
+        first = [n + ',' for n in namelist[:-2]]
+        first.append(lastTwo)
+        return " ".join(first)
+
+print(namelist([{'name': 'Bart'},{'name': 'Lisa'},{'name': 'Maggie'}]))
+# Bart, Lisa & Maggie
